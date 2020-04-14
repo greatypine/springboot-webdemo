ea7fd7897636c7801df4f74f092ed1132a08164e
caopeisong <caopeisong@haha.com>
2020-04-14 22:03:59 +0800

    first commit

---
+++ src/test/java/com/happywork/springbootwebdemo/SpringbootWebdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happywork.springbootwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
